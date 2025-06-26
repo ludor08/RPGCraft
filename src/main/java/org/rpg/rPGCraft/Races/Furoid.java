7da59a1ed717bf9055290cf3c3c584aaa779ffba
ludor08 <103389542+ludor08@users.noreply.github.com>
2025-06-26 11:12:10 -0700

    removed Feloid from Furoid

--- src/main/java/org/rpg/rPGCraft/Races/Furoid.java
+++ src/main/java/org/rpg/rPGCraft/Races/Furoid.java
@@ -16,6 +16,6 @@ public class Furoid extends Race
     // name of the race
     public Furoid(Main main)
     {
-        super("Furoid", ChatColor.GRAY, Material.BEEF, List.of(new Fur(), new AnimalAgility(main)),List.of(new Feloid(), new Foxoid(main)));
+        super("Furoid", ChatColor.GRAY, Material.BEEF, List.of(new Fur(), new AnimalAgility(main)),List.of(new Foxoid(main)));
     }
 }
