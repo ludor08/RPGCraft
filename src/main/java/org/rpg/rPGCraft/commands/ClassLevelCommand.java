954b189ac6cca9de4a3e773fa193999758e6c11d
ludor08 <103389542+ludor08@users.noreply.github.com>
2025-07-09 14:02:31 -0700

    added the ClassLevelCommand

---
+++ src/main/java/org/rpg/rPGCraft/commands/ClassLevelCommand.java
@@ -0,0 +1,107 @@
+package org.rpg.rPGCraft.commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.persistence.PersistentDataType;
+import org.jetbrains.annotations.NotNull;
+import org.rpg.rPGCraft.Main;
+
+public class ClassLevelCommand implements CommandExecutor
+{
+    Main main;
+
+    public ClassLevelCommand(Main main)
+    {
+        this.main = main;
+    }
+
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] args)
+    {
+        if (commandSender instanceof Player player)
+        {
+            if (args.length == 0)
+            {
+                player.sendMessage(ChatColor.DARK_RED + "ERROR : No arguments given. Please give a argument");
+                return false;
+            }
+
+            switch (args[0])
+            {
+                case "get":
+                    player.sendMessage(player.getPersistentDataContainer().get(main.GetLevelKey(), PersistentDataType.INTEGER).toString());
+                    break;
+
+                case "set":
+                    if (args.length > 1)
+                    {
+                        try
+                        {
+                            // if the player would have too much xp when the level is changed
+                            if (player.getPersistentDataContainer().get(main.GetClassXPKey(), PersistentDataType.INTEGER) > main.statSheetManager.GetLevelXPRequirements(Integer.parseInt(args[1])))
+                            {
+                                // set the players xp to 1 less than the max xp for that level
+                                player.getPersistentDataContainer().set(main.GetClassXPKey(), PersistentDataType.INTEGER, main.statSheetManager.GetLevelXPRequirements(Integer.parseInt(args[1]))-1);
+                            }
+
+                            player.getPersistentDataContainer().set(main.GetLevelKey(), PersistentDataType.INTEGER, Integer.parseInt(args[1]));
+
+                            player.sendMessage(ChatColor.GREEN + "class level set to " + Integer.parseInt(args[1]));
+                        }
+                        catch (NumberFormatException e)
+                        {
+                            player.sendMessage(ChatColor.DARK_RED + "ERROR : " + args[1] + " is not a valid integer.");
+                        }
+                        catch (IndexOutOfBoundsException e)
+                        {
+                            player.sendMessage(ChatColor.DARK_RED + "ERROR : " + args[1] + " is not a valid level.");
+                        }
+                    }
+                    else
+                    {
+                        player.sendMessage(ChatColor.DARK_RED + "ERROR : No second argument given. Please give a second argument");
+                    }
+
+                    break;
+
+                case "add":
+                    if (args.length > 1)
+                    {
+                        try
+                        {
+                            // if the player would have too much xp when the level is changed
+                            if (player.getPersistentDataContainer().get(main.GetClassXPKey(), PersistentDataType.INTEGER) > main.statSheetManager.GetLevelXPRequirements(Integer.parseInt(args[1]) + player.getPersistentDataContainer().get(main.GetLevelKey(), PersistentDataType.INTEGER)))
+                            {
+                                // set the players xp to 1 less than the max xp for that level
+                                player.getPersistentDataContainer().set(main.GetClassXPKey(), PersistentDataType.INTEGER, main.statSheetManager.GetLevelXPRequirements(Integer.parseInt(args[1]) + player.getPersistentDataContainer().get(main.GetLevelKey(), PersistentDataType.INTEGER)-1));
+                            }
+
+                            player.getPersistentDataContainer().set(main.GetLevelKey(), PersistentDataType.INTEGER,  player.getPersistentDataContainer().get(main.GetLevelKey(), PersistentDataType.INTEGER) + Integer.parseInt(args[1]));
+
+                            player.sendMessage(ChatColor.GREEN.toString() + args[1] + " added to your level. your total class level is now " + player.getPersistentDataContainer().get(main.GetLevelKey(), PersistentDataType.INTEGER));
+                        }
+                        catch (NumberFormatException e)
+                        {
+                            player.sendMessage(ChatColor.DARK_RED + "ERROR : " + args[1] + " is not a valid integer.");
+                        }
+                        catch (IndexOutOfBoundsException e)
+                        {
+                            player.sendMessage(ChatColor.DARK_RED + "ERROR : " + args[1] + " is not a valid level.");
+                        }
+                    }
+                    else
+                    {
+                        player.sendMessage(ChatColor.DARK_RED + "ERROR : No second argument given. Please give a second argument");
+                    }
+                    break;
+            }
+        }
+
+        return false;
+    }
+
+}
