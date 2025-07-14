5a550c3b783675a577113aad4c149d36e26c3bec
ludor08 <103389542+ludor08@users.noreply.github.com>
2025-07-14 15:10:53 -0700

    added the second level of vitality to the berserker

--- src/main/java/org/rpg/rPGCraft/Classes/Berserker.java
+++ src/main/java/org/rpg/rPGCraft/Classes/Berserker.java
@@ -8,7 +8,8 @@ import org.rpg.rPGCraft.Node;
 import org.rpg.rPGCraft.PlayableClass;
 import org.rpg.rPGCraft.TraitTree;
 import org.rpg.rPGCraft.Traits.Question_Axer;
-import org.rpg.rPGCraft.Traits.Vitality;
+import org.rpg.rPGCraft.Traits.Vitality.Vitality_1;
+import org.rpg.rPGCraft.Traits.Vitality.Vitality_2;
 
 import java.util.List;
 
@@ -18,8 +19,8 @@ public class Berserker extends PlayableClass
     public Berserker(Main main)
     {
         super("Berserker", ChatColor.RED, Material.IRON_AXE, List.of(ChatColor.AQUA + "Close range fight that can take a beating, and give it right back"), new TraitTree(List.of(
-                new Node(new Vector2d(4,0), new Vitality(main)),
-                new Node(new Vector2d(4,1), new Question_Axer(main))
+                new Node(new Vector2d(4,0), List.of(new Vitality_1(main), new Vitality_2(main))),
+                new Node(new Vector2d(4,1), List.of(new Question_Axer(main)))
         )));
     }
 }
