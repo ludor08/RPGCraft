432a2a0c4e7a419ef06c5bc39471f1bd0b9718b1
ludor08 <103389542+ludor08@users.noreply.github.com>
2025-07-24 01:00:56 -0700

    added rage to Berserker

--- src/main/java/org/rpg/rPGCraft/Classes/Berserker.java
+++ src/main/java/org/rpg/rPGCraft/Classes/Berserker.java
@@ -7,9 +7,10 @@ import org.rpg.rPGCraft.Main;
 import org.rpg.rPGCraft.Node;
 import org.rpg.rPGCraft.PlayableClass;
 import org.rpg.rPGCraft.TraitTree;
-import org.rpg.rPGCraft.Traits.Question_Axer;
-import org.rpg.rPGCraft.Traits.Vitality.Vitality_1;
-import org.rpg.rPGCraft.Traits.Vitality.Vitality_2;
+import org.rpg.rPGCraft.Traits.Active.Rage;
+import org.rpg.rPGCraft.Traits.Passive.Question_Axer;
+import org.rpg.rPGCraft.Traits.Passive.Vitality.Vitality_1;
+import org.rpg.rPGCraft.Traits.Passive.Vitality.Vitality_2;
 
 import java.util.List;
 
@@ -18,9 +19,10 @@ public class Berserker extends PlayableClass
     // name of the race
     public Berserker(Main main)
     {
-        super("Berserker", ChatColor.RED, Material.IRON_AXE, List.of(ChatColor.AQUA + "Close range fight that can take a beating, and give it right back"), new TraitTree(List.of(
+        super("Berserker", ChatColor.RED, Material.IRON_AXE, List.of(ChatColor.AQUA + "Close range fight that can take a beating, and give it right back."), new TraitTree(List.of(
                 new Node(new Vector2d(4,0), List.of(new Vitality_1(main), new Vitality_2(main))),
-                new Node(new Vector2d(4,1), List.of(new Question_Axer(main)))
+                new Node(new Vector2d(3,1), List.of(new Question_Axer(main))),
+                new Node(new Vector2d(4,1), List.of(new Rage(main)))
         )));
     }
 }
