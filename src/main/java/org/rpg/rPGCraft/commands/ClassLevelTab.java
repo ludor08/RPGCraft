775cbc98336015db73d5bf48a7f8be0c1e8b4634
ludor08 <103389542+ludor08@users.noreply.github.com>
2025-08-26 12:15:27 -0700

    added tab complete to the class level command

---
+++ src/main/java/org/rpg/rPGCraft/commands/ClassLevelTab.java
@@ -0,0 +1,26 @@
+package org.rpg.rPGCraft.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.util.StringUtil;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ClassLevelTab implements TabCompleter
+{
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] args)
+    {
+        if (args.length == 1)
+        {
+            return StringUtil.copyPartialMatches(args[0], Arrays.asList("get", "set", "add"), new ArrayList<>());
+        }
+
+        return List.of();
+    }
+}
