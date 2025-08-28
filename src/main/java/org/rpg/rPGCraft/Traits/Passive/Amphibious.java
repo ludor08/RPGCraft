74988fa883d7bf80780ebb4e058cc42ea89f5946
ludor08 <103389542+ludor08@users.noreply.github.com>
2025-08-28 16:13:27 -0700

    Made crab mine faster underwater

--- src/main/java/org/rpg/rPGCraft/Traits/Passive/Amphibious.java
+++ src/main/java/org/rpg/rPGCraft/Traits/Passive/Amphibious.java
@@ -1,31 +1,28 @@
 package org.rpg.rPGCraft.Traits.Passive;
 
-import com.destroystokyo.paper.event.player.PlayerJumpEvent;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeModifier;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.entity.FoodLevelChangeEvent;
-import org.bukkit.event.player.PlayerRespawnEvent;
-import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.rpg.rPGCraft.Main;
+import org.rpg.rPGCraft.RPGutils;
 import org.rpg.rPGCraft.Trait;
 
 import java.util.List;
 
 public class Amphibious extends Trait
 {
+    AttributeModifier submergedMiningSpeedMod = new AttributeModifier(new NamespacedKey(main, "amphibious_submerged_mining_speed"), 1, AttributeModifier.Operation.ADD_NUMBER);
+
     public Amphibious(Main main) {
         // add the name and lore
         super("Amphibious", "amphibious", ChatColor.AQUA, Material.POTION, true, main, List.of(
-                ChatColor.AQUA.toString() + "   - Gains the conduit power effect."
+                ChatColor.AQUA.toString() + "   - Gains the conduit power effect.",
+                ChatColor.AQUA.toString() + "   - Makes the player mine even faster underwater."
         ));
     }
 
@@ -33,5 +30,17 @@ public class Amphibious extends Trait
     public void OnTick(Player player)
     {
         player.addPotionEffect(new PotionEffect(PotionEffectType.CONDUIT_POWER, 50, 0));
+    }
+
+    @Override
+    public void OnGainTraitBuff(Player player)
+    {
+        RPGutils.SafeAttributeAdd(Attribute.SUBMERGED_MINING_SPEED, submergedMiningSpeedMod,player);
+    }
+
+    @Override
+    public void OnRemoveTraitBuff(Player player)
+    {
+        RPGutils.SafeAttributeRemove(Attribute.SUBMERGED_MINING_SPEED, submergedMiningSpeedMod,player);
     }
 }
