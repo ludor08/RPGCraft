22fbe6ce206002fcadd6c52297e7f6dee3bd09b5
ludor08 <103389542+ludor08@users.noreply.github.com>
2025-08-28 16:24:33 -0700

    Made the insect have less hp

--- src/main/java/org/rpg/rPGCraft/Races/Subraces/Insectoid.java
+++ src/main/java/org/rpg/rPGCraft/Races/Subraces/Insectoid.java
@@ -5,6 +5,7 @@ import org.bukkit.Material;
 import org.rpg.rPGCraft.Main;
 import org.rpg.rPGCraft.Race;
 import org.rpg.rPGCraft.Traits.Passive.Size_Change_Tiny;
+import org.rpg.rPGCraft.Traits.Passive.Squashable;
 import org.rpg.rPGCraft.Traits.Passive.Wings;
 
 import java.util.List;
@@ -14,6 +15,6 @@ public class Insectoid extends Race
     // set up the race
     public Insectoid(Main main)
     {
-        super("Insectoid", ChatColor.GRAY, Material.ARMADILLO_SCUTE, List.of(new Size_Change_Tiny(main), new Wings(main)),null);
+        super("Insectoid", ChatColor.GRAY, Material.ARMADILLO_SCUTE, List.of(new Size_Change_Tiny(main), new Wings(main), new Squashable(main)),null);
     }
 }
---
+++ src/main/java/org/rpg/rPGCraft/Traits/Passive/Squashable.java
@@ -0,0 +1,39 @@
+package org.rpg.rPGCraft.Traits.Passive;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.entity.Player;
+import org.rpg.rPGCraft.Main;
+import org.rpg.rPGCraft.RPGutils;
+import org.rpg.rPGCraft.Trait;
+
+import java.util.List;
+
+public class Squashable extends Trait
+{
+    private final AttributeModifier maxHealthMod = new AttributeModifier(new NamespacedKey(main, "squashable_max_health"), -10, AttributeModifier.Operation.ADD_NUMBER);
+
+    public Squashable(Main main)
+    {
+        // add the name and lore
+        super("Squashable", "squashable", ChatColor.AQUA, Material.REDSTONE, false, main, List.of(
+                ChatColor.AQUA.toString() + "   - Sets your base max health to 10."
+        ));
+
+    }
+
+    @Override
+    public void OnGainTraitBuff(Player player)
+    {
+        RPGutils.SafeAttributeAdd(Attribute.MAX_HEALTH, maxHealthMod,player);
+    }
+
+    @Override
+    public void OnRemoveTraitBuff(Player player)
+    {
+        RPGutils.SafeAttributeRemove(Attribute.MAX_HEALTH, maxHealthMod,player);
+    }
+}
