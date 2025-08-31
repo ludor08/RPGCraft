1ec1aa1ed2b9d066d7b767e2f5059c2d07e6ef15
ludor08 <103389542+ludor08@users.noreply.github.com>
2025-08-30 19:19:07 -0700

    Sage class now has traits up to level 10

--- src/main/java/org/rpg/rPGCraft/Classes/Sage.java
+++ src/main/java/org/rpg/rPGCraft/Classes/Sage.java
@@ -7,13 +7,16 @@ import org.rpg.rPGCraft.Main;
 import org.rpg.rPGCraft.Node;
 import org.rpg.rPGCraft.PlayableClass;
 import org.rpg.rPGCraft.TraitTree;
+import org.rpg.rPGCraft.Traits.Active.AuraOfWeakness;
 import org.rpg.rPGCraft.Traits.Active.MendMinorWounds;
+import org.rpg.rPGCraft.Traits.Passive.Boosted.Healing.BoostedHealing_speed;
 import org.rpg.rPGCraft.Traits.Passive.EmpoweredMixture.EmpoweredMixture_1;
 import org.rpg.rPGCraft.Traits.Passive.EnhancedMixture.EnhancedMixture_1;
 import org.rpg.rPGCraft.Traits.Passive.EnhancedMixture.EnhancedMixture_2;
 import org.rpg.rPGCraft.Traits.Passive.GreaterCapacity.GreaterCapacity_1;
 import org.rpg.rPGCraft.Traits.Passive.GreaterCapacity.GreaterCapacity_2;
 import org.rpg.rPGCraft.Traits.Passive.ManaRegainSpeed.ManaRegainSpeed;
+import org.rpg.rPGCraft.Traits.Passive.RebukeOfTheFlame;
 
 import java.util.List;
 
@@ -23,11 +26,15 @@ public class Sage extends PlayableClass
     public Sage(Main main)
     {
         super("Sage", ChatColor.LIGHT_PURPLE, Material.STICK, List.of(ChatColor.AQUA + "Uses spells and brews potions to heal, buff, and debuff."), new TraitTree(List.of(
-                new Node(new Vector2d(4,0), List.of(new GreaterCapacity_1(main), new GreaterCapacity_2(main)), "000"),
-                new Node(new Vector2d(4,1), List.of(new EnhancedMixture_1(main), new EnhancedMixture_2(main)), "000"),
-                new Node(new Vector2d(4,2), List.of(new ManaRegainSpeed(main)), "000"),
-                new Node(new Vector2d(4,3), List.of(new MendMinorWounds(main)), "000"),
-                new Node(new Vector2d(4,4), List.of(new EmpoweredMixture_1(main)), "000")
+                new Node(new Vector2d(2,2), List.of(new GreaterCapacity_1(main)), "000"),
+                new Node(new Vector2d(1,1), List.of(new BoostedHealing_speed(main)), "000"),
+                new Node(new Vector2d(1,2), List.of(new RebukeOfTheFlame(main)), "000"),
+                new Node(new Vector2d(3,1), List.of(new GreaterCapacity_1(main)), "001"),
+                new Node(new Vector2d(5,1), List.of(new EnhancedMixture_1(main), new EnhancedMixture_2(main)), "000"),
+                new Node(new Vector2d(4,0), List.of(new ManaRegainSpeed(main)), "000"),
+                new Node(new Vector2d(4,1), List.of(new MendMinorWounds(main)), "000"),
+                new Node(new Vector2d(5,2), List.of(new EmpoweredMixture_1(main)), "000"),
+                new Node(new Vector2d(3,2), List.of(new AuraOfWeakness(main)), "000")
         )));
     }
 }
