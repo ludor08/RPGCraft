f3350efb772e0b6eecd46a7c1a52161696bf1c24
ludor08 <103389542+ludor08@users.noreply.github.com>
2025-09-05 12:12:23 -0700

    Mend Minor Wounds "Shoots a beam that heals whichever entity it hits for three hearts." -> "Shoots a beam that heals whichever entity it hits for five health. If the beam doesn't hit anything, heal yourself instead."

--- src/main/java/org/rpg/rPGCraft/Traits/Active/MendMinorWounds.java
+++ src/main/java/org/rpg/rPGCraft/Traits/Active/MendMinorWounds.java
@@ -1,12 +1,11 @@
 package org.rpg.rPGCraft.Traits.Active;
 
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
-import org.bukkit.Particle;
+import org.bukkit.*;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityRegainHealthEvent;
+import org.bukkit.persistence.PersistentDataType;
 import org.joml.Vector3d;
 import org.rpg.rPGCraft.ActiveTrait;
 import org.rpg.rPGCraft.Main;
@@ -16,10 +15,14 @@ import java.util.List;
 
 public class MendMinorWounds extends ActiveTrait
 {
+    private final NamespacedKey healAmountKey = new NamespacedKey(main, "mend_minor_wounds_amount");
+    int baseHealAmount = 5;
+
     public MendMinorWounds(Main main) {
         // add the name and lore
         super("Mend Minor Wounds", "mend minor wounds", 35, ChatColor.GREEN, Material.TURTLE_SCUTE, false, main, List.of(
-                ChatColor.AQUA.toString() + "   - Shoots a beam that heals whichever entity it hits for three hearts."
+                ChatColor.AQUA.toString() + "   - Shoots a beam that heals whichever entity it hits for five health.",
+                ChatColor.AQUA.toString() + "     If the beam doesn't hit anything, heal yourself instead."
         ));
     }
 
@@ -33,11 +36,45 @@ public class MendMinorWounds extends ActiveTrait
     public void TriggerActiveEvent(Player player)
     {
         Vector3d direction = new Vector3d(-Math.cos(Math.toRadians(player.getPitch())) * Math.sin(Math.toRadians(player.getYaw())), -Math.sin(Math.toRadians(player.getPitch())), Math.cos(Math.toRadians(player.getPitch())) * Math.cos(Math.toRadians(player.getYaw())));
-        Entity lookingAt = RPGutils.RecastForEntity(100, direction, player.getEyeLocation(), true, player, Particle.HAPPY_VILLAGER, 5);
+        Entity lookingAt = RPGutils.RecastForEntity(100, direction, player.getEyeLocation(), true, player, null, 0);
 
         if (lookingAt instanceof LivingEntity livingLookingAt)
         {
-            RPGutils.HealWithTraits(player,livingLookingAt,6,EntityRegainHealthEvent.RegainReason.MAGIC,main);
+            RPGutils.HealWithTraits(player,livingLookingAt,player.getPersistentDataContainer().get(healAmountKey, PersistentDataType.INTEGER),EntityRegainHealthEvent.RegainReason.MAGIC,main);
+            RPGutils.RecastForEntity(100, direction, player.getEyeLocation(), true, player, Particle.HAPPY_VILLAGER, 5);
+        }
+        else if (lookingAt == null)
+        {
+            RPGutils.HealWithTraits(player,player,player.getPersistentDataContainer().get(healAmountKey, PersistentDataType.INTEGER),EntityRegainHealthEvent.RegainReason.MAGIC,main);
+            for (int i = 1; i < 20; i++)
+            {
+                Vector3d offset = new Vector3d(Math.cos((Math.PI*2)/((double) i /20)), i*0.1, Math.sin((Math.PI*2)/((double) i /20)));
+                Location location = new Location(player.getWorld(), player.getLocation().getX() + offset.x, player.getLocation().getY() + offset.y, player.getLocation().getZ() + offset.z);
+
+                player.getWorld().spawnParticle(Particle.HAPPY_VILLAGER, location, 10, 0,0,0,0);
+            }
+        }
+    }
+
+    @Override
+    public void OnRemoveTraitBuff(Player player)
+    {
+        if (player.getPersistentDataContainer().has(healAmountKey))
+        {
+            player.getPersistentDataContainer().set(healAmountKey, PersistentDataType.INTEGER, player.getPersistentDataContainer().get(healAmountKey, PersistentDataType.INTEGER) - baseHealAmount);
+        }
+    }
+
+    @Override
+    public void OnGainTraitBuff(Player player)
+    {
+        if (player.getPersistentDataContainer().has(healAmountKey))
+        {
+            player.getPersistentDataContainer().set(healAmountKey, PersistentDataType.INTEGER, player.getPersistentDataContainer().get(healAmountKey, PersistentDataType.INTEGER) + baseHealAmount);
+        }
+        else
+        {
+            player.getPersistentDataContainer().set(healAmountKey, PersistentDataType.INTEGER, baseHealAmount);
         }
     }
 }
