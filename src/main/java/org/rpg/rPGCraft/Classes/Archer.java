7efe71b99230e41e168ab458f822aa7df1c36842
ludor08 <103389542+ludor08@users.noreply.github.com>
2025-09-21 13:04:46 -0700

    Archer class now has traits up to level 20.

--- src/main/java/org/rpg/rPGCraft/Classes/Archer.java
+++ src/main/java/org/rpg/rPGCraft/Classes/Archer.java
@@ -8,10 +8,16 @@ import org.rpg.rPGCraft.Node;
 import org.rpg.rPGCraft.PlayableClass;
 import org.rpg.rPGCraft.TraitTree;
 import org.rpg.rPGCraft.Traits.Active.GrapplingHookArrow;
+import org.rpg.rPGCraft.Traits.Active.PinShot;
 import org.rpg.rPGCraft.Traits.Active.SteadyAim;
+import org.rpg.rPGCraft.Traits.CostModifier.CheaperGrapplingHook;
+import org.rpg.rPGCraft.Traits.CostModifier.CheaperSteadyAim;
+import org.rpg.rPGCraft.Traits.CostModifier.EvenCheaperGrapplingHook;
+import org.rpg.rPGCraft.Traits.Passive.*;
+import org.rpg.rPGCraft.Traits.Passive.GreaterCapacity.GreaterCapacity_1;
 import org.rpg.rPGCraft.Traits.Passive.Headshot.BetterHeadshots;
 import org.rpg.rPGCraft.Traits.Passive.Headshot.Headshot;
-import org.rpg.rPGCraft.Traits.Passive.Ricochet;
+import org.rpg.rPGCraft.Traits.Passive.Vitality.Vitality_1;
 
 import java.util.List;
 
@@ -22,10 +28,25 @@ public class Archer extends PlayableClass
     {
         super("Archer", ChatColor.BLUE, Material.BOW, List.of(ChatColor.AQUA + "Uses long range weapons of all kinds."), new TraitTree(List.of(
                 new Node(new Vector2d(4,0), List.of(new Headshot(main)), "000"),
-                new Node(new Vector2d(4,1), List.of(new GrapplingHookArrow(main)), "000"),
-                new Node(new Vector2d(4,2), List.of(new BetterHeadshots(main)), "000"),
-                new Node(new Vector2d(4,3), List.of(new Ricochet(main)), "000"),
-                new Node(new Vector2d(4,4), List.of(new SteadyAim(main)), "000")
+                new Node(new Vector2d(3,2), List.of(new GrapplingHookArrow(main)), "000"),
+                new Node(new Vector2d(3,3), List.of(new CheaperGrapplingHook(main)), "000"),
+                new Node(new Vector2d(4,1), List.of(new BetterHeadshots(main)), "000"),
+                new Node(new Vector2d(3,1), List.of(new Ricochet(main)), "000"),
+                new Node(new Vector2d(5,1), List.of(new SteadyAim(main)), "000"),
+                new Node(new Vector2d(2,4), List.of(new PowerShot(main)), "000"),
+                new Node(new Vector2d(2,5), List.of(new TeleportingGrapple(main)), "000"),
+                new Node(new Vector2d(3,5), List.of(new PinShot(main)), "000"),
+                new Node(new Vector2d(5,2), List.of(new LaserShot(main)), "000"),
+                new Node(new Vector2d(5,3), List.of(new PowerShot(main)), "001"),
+                new Node(new Vector2d(2,3), List.of(new WindShot(main)), "000"),
+                new Node(new Vector2d(2,6), List.of(new HighPowerWindShot(main)), "000"),
+                new Node(new Vector2d(6,3), List.of(new CheaperSteadyAim(main)), "000"),
+                new Node(new Vector2d(6,4), List.of(new EvenCheaperGrapplingHook(main)), "000"),
+                new Node(new Vector2d(6,5), List.of(new CorrectiveWindResistance(main)), "000"),
+                new Node(new Vector2d(4,5), List.of(new GreaterCapacity_1(main)), "000"),
+                new Node(new Vector2d(3,5), List.of(new BloodyRetreat(main)), "000"),
+                new Node(new Vector2d(5,5), List.of(new Vitality_1(main)), "000"),
+                new Node(new Vector2d(6,6), List.of(new AntiGravityArrow(main)), "000")
         )));
     }
 }
