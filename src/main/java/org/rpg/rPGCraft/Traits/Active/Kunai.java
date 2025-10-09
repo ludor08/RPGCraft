721c6eff7e5c891bca6ae9da54cd9757cdece655
ludor08 <103389542+ludor08@users.noreply.github.com>
2025-10-09 00:36:50 -0700

    Kunai “Throw a kunai that does 1 damage and can carry other effects.”

---
+++ src/main/java/org/rpg/rPGCraft/Traits/Active/Kunai.java
@@ -0,0 +1,78 @@
+package org.rpg.rPGCraft.Traits.Active;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.rpg.rPGCraft.ActiveTrait;
+import org.rpg.rPGCraft.Main;
+import org.rpg.rPGCraft.RPGutils;
+
+import java.util.List;
+
+public class Kunai extends ActiveTrait
+{
+    NamespacedKey kunaiKey = new NamespacedKey(main, "kunai");
+
+    NamespacedKey poisonKunaiKey = new NamespacedKey(main, "kunai_poison");
+    NamespacedKey poisonLevelKey = new NamespacedKey(main, "kunai_poison_level");
+    NamespacedKey poisonDurationKey = new NamespacedKey(main, "kunai_poison_duration");
+
+    public Kunai(Main main) {
+        // add the name and lore
+        super("Kunai", "kunai", 5, ChatColor.RED, Material.STONE_SWORD, false, main, List.of(
+                ChatColor.AQUA.toString() + "   - Throw a kunai that does 1 damage and can carry other effects."
+        ));
+
+
+    }
+
+    @Override
+    public String GetInputSequence()
+    {
+        return "011";
+    }
+
+    @Override
+    public void TriggerActiveEvent(Player player)
+    {
+        Arrow kunai = player.launchProjectile(Arrow.class);
+        kunai.getPersistentDataContainer().set(kunaiKey, PersistentDataType.BOOLEAN, true);
+
+        ((LivingEntity)kunai).getAttribute(Attribute.SCALE).addModifier(new AttributeModifier(new NamespacedKey(main, "kunai_size"), -0.9d, AttributeModifier.Operation.ADD_NUMBER));
+        kunai.setDamage(1);
+        kunai.setVelocity(kunai.getVelocity().multiply(2));
+        kunai.setCustomName("Kunai");
+        kunai.setGravity(false);
+
+        if (player.getPersistentDataContainer().has(kunaiKey))
+        {
+            RPGutils.SetNamespacedKeyValue(kunai, poisonKunaiKey, player.getPersistentDataContainer().has(poisonKunaiKey));
+            RPGutils.SetNamespacedKeyValue(kunai, poisonLevelKey, player.getPersistentDataContainer().get(poisonLevelKey, PersistentDataType.INTEGER));
+            RPGutils.SetNamespacedKeyValue(kunai, poisonDurationKey, player.getPersistentDataContainer().get(poisonDurationKey, PersistentDataType.INTEGER));
+        }
+    }
+
+    @Override
+    public void OnShotProjectileHit(ProjectileHitEvent e)
+    {
+        if (e.getEntity().getPersistentDataContainer().has(kunaiKey))
+        {
+            if (e.getEntity().getPersistentDataContainer().get(poisonKunaiKey, PersistentDataType.BOOLEAN))
+            {
+                if (e.getHitEntity() != null)
+                {
+                    if (e.getHitEntity() instanceof LivingEntity livingHit) livingHit.addPotionEffect(new PotionEffect(PotionEffectType.POISON, e.getEntity().getPersistentDataContainer().get(poisonDurationKey, PersistentDataType.INTEGER),e.getEntity().getPersistentDataContainer().get(poisonLevelKey, PersistentDataType.INTEGER)-1));
+                }
+            }
+        }
+    }
+}
