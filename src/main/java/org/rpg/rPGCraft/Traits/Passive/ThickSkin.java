7b240b03b793420b88147601db19a144e895aaa3
ludor08 <103389542+ludor08@users.noreply.github.com>
2025-11-03 00:57:58 -0800

    Thick Skin "Takes 10% less damage from all damage sources."

---
+++ src/main/java/org/rpg/rPGCraft/Traits/Passive/ThickSkin.java
@@ -0,0 +1,46 @@
+package org.rpg.rPGCraft.Traits.Passive;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.entity.Player;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.persistence.PersistentDataType;
+import org.rpg.rPGCraft.Main;
+import org.rpg.rPGCraft.RPGutils;
+import org.rpg.rPGCraft.Trait;
+
+import java.util.List;
+
+public class ThickSkin extends Trait
+{
+    private final NamespacedKey otherDamageKey = new NamespacedKey(main, "flash_of_oak_other_damage");
+    float otherDamageMod = 0.1f;
+
+    public ThickSkin(Main main)
+    {
+        // add the name and lore
+        super("Thick Skin", "thick skin", ChatColor.AQUA, Material.ARMADILLO_SCUTE, false, main, List.of(
+                ChatColor.AQUA.toString() + "   - Takes 10% less damage from all damage sources."
+        ));
+    }
+
+    @Override
+    public void OnTakeDamage(EntityDamageEvent e)
+    {
+        e.setDamage(e.getDamage() * 1 - e.getEntity().getPersistentDataContainer().get(otherDamageKey, PersistentDataType.FLOAT));
+    }
+
+    @Override
+    public void OnRemoveTraitBuff(Player player)
+    {
+        RPGutils.RemoveNamespacedKey(player, otherDamageKey);
+    }
+
+    @Override
+    public void OnGainTraitBuff(Player player)
+    {
+        RPGutils.AddToNamespacedKey(player, otherDamageKey, 0, otherDamageMod);
+    }
+}
