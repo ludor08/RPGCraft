20630aece396e8d4556014bff9b312b99b89bbe0
ludor08 <103389542+ludor08@users.noreply.github.com>
2026-01-17 15:06:36 -0800

    Added particle to anti gravity arrow

--- src/main/java/org/rpg/rPGCraft/Traits/Passive/AntiGravityArrow.java
+++ src/main/java/org/rpg/rPGCraft/Traits/Passive/AntiGravityArrow.java
@@ -2,17 +2,27 @@ package org.rpg.rPGCraft.Traits.Passive;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.Particle;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.entity.ProjectileLaunchEvent;
+import org.bukkit.persistence.PersistentDataType;
+import org.joml.Vector3d;
 import org.rpg.rPGCraft.Main;
-import org.rpg.rPGCraft.Trait;
+import org.rpg.rPGCraft.RPGparticles;
+import org.rpg.rPGCraft.Traits.Trait;
 
 import java.util.List;
 
 public class AntiGravityArrow extends Trait
 {
-    public AntiGravityArrow(Main main) {
+    NamespacedKey antiGravityKey = new NamespacedKey(Main.GetInstance(), "anti_gravity");
+
+    public AntiGravityArrow() {
         // add the name and lore
-        super("Anti-Gravity Arrow", "anti gravity arrow", ChatColor.AQUA, Material.ARROW, false, main, List.of(
+        super(ChatColor.AQUA + ChatColor.BOLD.toString() + "Anti-Gravity Arrow", "anti gravity arrow", Material.ARROW, true, List.of(
                 ChatColor.AQUA.toString() + "   - Projectiles have gravity.",
                 ChatColor.AQUA.toString() + "   - Projectiles launch at 50% of the normal speed."
         ));
@@ -22,5 +32,25 @@ public class AntiGravityArrow extends Trait
     public void OnLaunchProjectile(ProjectileLaunchEvent e)
     {
         e.getEntity().setGravity(false);
+
+        e.getEntity().getPersistentDataContainer().set(antiGravityKey, PersistentDataType.BOOLEAN, true);
+    }
+
+    @Override
+    public void OnTick(Player player)
+    {
+        for (Entity entity : player.getWorld().getEntities())
+        {
+            if (entity instanceof Projectile projectile)
+            {
+                if (projectile.getShooter() == player)
+                {
+                    if (projectile.getPersistentDataContainer().has(antiGravityKey))
+                    {
+                        RPGparticles.SpawnParticle(player.getWorld(), 50, projectile.getLocation(), new Vector3d(0,0,0), Particle.ENCHANT, 0);
+                    }
+                }
+            }
+        }
     }
 }
