5398e4ca8d330aaaace2c9e7862cfd2eaed4fda4
ludor08 <103389542+ludor08@users.noreply.github.com>
2026-03-25 02:13:30 -0700

    Added a BossRoomWorldProperties

---
+++ src/main/java/org/rpg/rPGCraft/WorldProperties/BossRoomWorldProperties.java
@@ -0,0 +1,21 @@
+package org.rpg.rPGCraft.WorldProperties;
+
+import org.bukkit.World;
+import org.bukkit.WorldCreator;
+import org.bukkit.WorldType;
+
+public class BossRoomWorldProperties extends WorldProperties
+{
+    public BossRoomWorldProperties()
+    {
+        super("world_boss_room", new WorldCreator("world_boss_room")
+                .generateStructures(false)
+                .type(WorldType.FLAT)
+                .generatorSettings("{\"layers\": [{\"block\": \"minecraft:air\", \"height\": 1}], \"biome\": \"plains\"}"));
+    }
+
+    public void OnTick()
+    {
+
+    }
+}
